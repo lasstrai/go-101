824c4dc9abaf35fa10f2a11fcacc4364c93765de
lasstrai <user7x9@gmail.com>
2024-11-12 11:25:44 -0500

    leetcode 35.searchInsert pending

---
+++ leetcode/binary-search/35.go
@@ -0,0 +1,18 @@
+package main
+
+func searchInsert(nums []int, target int) int {
+	low := 0
+	high := len(nums) - 1
+	var mid int
+	for low <= high {
+		mid = low + ((high - low) / 2)
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+	return mid + 1
+}
